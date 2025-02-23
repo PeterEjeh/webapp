0408d2f636244c76608c73768cf97e103322fb86
Peter Ejeh <peterejeh09@gmail.com>
2025-02-23 17:13:34 +0000

    recent updates

--- petesgram/petesgram/settings.py
+++ petesgram/petesgram/settings.py
@@ -135,8 +135,9 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'successp560@gmail.com.com' 
-EMAIL_HOST_PASSWORD = '!Fe0luw@!13'  
+EMAIL_HOST_USER = 'peterejeh09@gmail.com'
+EMAIL_HOST_PASSWORD = 'edyk jtpn pmeb cefw '
+DEFAULT_FROM_EMAIL = 'PetesGram <peterejeh09@gmail.com>'
